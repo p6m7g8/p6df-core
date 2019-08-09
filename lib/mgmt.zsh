a5a0a4403dc5996b5a7fbcdc824107a41f7762bc
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-08-09 18:06:55 -0400

    sync

--- lib/mgmt.zsh
+++ lib/mgmt.zsh
@@ -15,15 +15,54 @@ p6df::mgmt::reload() {
 
 p6df::mgmt::modules::update() {
 
-  p6df::mgmt::iterator "git pull"
+  p6df::mgmt::_iterator "git pull"
 }
 
 p6df::mgmt::modules::diff() {
 
-  p6df::mgmt::iterator "git diff"
+  p6df::mgmt::_iterator "git diff"
 }
 
 p6df::mgmt::iterator() {
+    local include_p6="$1"
+
+    (
+      echo "============> $P6_DFZ_SRC_DIR"
+
+      cd $P6_DFZ_SRC_DIR
+
+      local orgs
+      if [[ $include_p6 = "p6" ]]; then
+	orgs=$(ls -1 | grep -v p6m7g8)
+      else
+	orgs=$(ls -1)
+      fi
+
+      local org
+      for org in $(echo $orgs); do
+	  (
+	    echo "========> $org"
+	    cd $P6_DFZ_SRC_DIR/$org
+	    local users=$(ls -1)
+
+	    local user
+	    for user in $(echo $users); do
+		(
+		    echo "=====> $org/$user"
+		    cd $P6_DFZ_SRC_DIR/$org/$user
+		    local crev=$(git log --pretty=format:"%h" -1)
+		    git pull
+		    local nrev=$(git log --pretty=format:"%h" -1)
+		    git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative ${crev}..${nrev}
+		)
+		return
+	    done
+	  )
+      done
+    )
+}
+
+p6df::mgmt::_iterator() {
   local cmd="$1"
   shift 1
 
