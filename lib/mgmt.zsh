2cd7756a9ae4ed0b1de61623a57ffacdc6aa86cc
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-06-17 14:27:52 -0400

    feat(mgmt): uses p6_git_p6_* functions, improves iterator()

--- lib/mgmt.zsh
+++ lib/mgmt.zsh
@@ -1,8 +1,9 @@
+# shellcheck shell=zsh
+
 p6df::mgmt::other::modules::update_all() {
 
-  p6df::mgmt::other::iterator "git pull"
+  p6df::mgmt::other::iterator "p6_git_p6_pulll"
 }
-
 
 p6df::mgmt::other::iterator() {
 
@@ -43,34 +44,57 @@ p6df::mgmt::reload() {
   p6df::init
 }
 
-p6df::mgmt::modules::pull() {
-
-  p6df::mgmt::iterator "git pull"
-}
-
 p6df::mgmt::modules::diff() {
 
-  p6df::mgmt::iterator "git diff"
+  p6df::mgmt::iterator "p6_git_p6_diff"
+}
+
+p6df::mgmt::modules::pull() {
+
+  p6df::mgmt::iterator "p6_git_p6_pull"
 }
 
 p6df::mgmt::modules::push() {
 
-  p6df::mgmt::iterator "git push"
+  p6df::mgmt::iterator "p6_git_p6_push"
+}
+
+p6df::mgmt::modules::sync() {
+
+  p6df::mgmt::iterator "p6_git_p6_sync"
 }
 
 p6df::mgmt::iterator() {
 
-    (
-	echo "==> $P6_DFZ_SRC_P6M7G8_DIR"
-	cd $P6_DFZ_SRC_P6M7G8_DIR
+	# Step one pull the repo repos
+	local remote_repos
+	remote_repos=$(p6_github_api_org_repos_list "https://api.github.com" "p6m7g8")
+
+	local local_repos
+	local_repos=$(p6_dir_list "$P6_DFZ_SRC_P6M7G8_DIR")
+
+	local unique_repos
+	unique_repos=$(echo "$local_repos" "$remote_repos" | sort -u)
+	
+	local repo
+	for repo in $(echo "$unique_repos"); do
+		p6_h1 "$repo"
+		p6_run_yield "p6df::mgmt::iterator::execute $P6_DFZ_SRC_P6M7G8_DIR/$repo $@"
+	done
+}
+
+p6df::mgmt::iterator::execute() {
+	local dir="$1"
+	shift 1
 
 	local repo
-	for repo in $(cd . ; ls -1); do
-	    (
-		echo "=====> $repo"
-		cd $P6_DFZ_SRC_P6M7G8_DIR/$repo
-		eval "$@"
-	    )
-	done
-    )
+	repo=$(basename "$dir")
+    if ! p6_dir_exists "$dir"; then
+	  gh repo clone p6m7g8/$repo $dir
+	fi
+
+	(
+		cd $dir	
+		p6_run_code "$@"
+	)
 }
