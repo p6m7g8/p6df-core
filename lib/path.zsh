4835047e480a10e5d9e4df12e93f8daf0988b471
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-18 10:55:29 -0400

    feat(env): adds P6_DFZ_SRC_ORIGINAL_DIR
    expands cdpath

--- lib/path.zsh
+++ lib/path.zsh
@@ -31,4 +31,15 @@ p6df::core::path::cd::init() {
   cdpath=()
   p6df::util::cdpath::if $P6_DFZ_SRC_DIR
   p6df::util::cdpath::if $P6_DFZ_SRC_P6M7G8_DIR
+  p6df::util::cdpath::if $P6_DFZ_SRC_WORK_DIR
+
+  p6df::util::cdpath::if $P6_DFZ_SRC_ORIGINAL_DIR
+  p6df::util::cdpath::if $P6_DFZ_SRC_ORIGINAL_DIR/p6m7g8
+  p6df::util::cdpath::if $P6_DFZ_SRC_ORIGINAL_DIR/pgollucci
+
+  p6df::util::cdpath::if $P6_DFZ_SRC_FOCUSED_DIR
+
+  p6df::util::cdpath::if $P6_DFZ_SRC_FORKED_DIR/pgollucci
+
+  p6df::util::cdpath::if $P6_DFZ_SRC_MISC_DIR
 }
